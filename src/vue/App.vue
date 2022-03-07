538484c1157817f7751974cdfd6fb86844a061d8
Douwe Gnodde <olvenmage@live.nl>
2022-03-07 15:44:27 +0100

    provider in app

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,10 +1,14 @@
 <template>
+  <WatchListProvider>
     <DefaultLayout>
       <router-view />
     </DefaultLayout>
+  </WatchListProvider>
 </template>
 
 <script setup lang="ts">
 import 'bootstrap/dist/css/bootstrap.css';
 import DefaultLayout from "./layouts/default/DefaultLayout.vue";
+import WatchListProvider from "../../components/WatchListProvider.vue";
+
 </script>
--- src/vue/layouts/default/DefaultLayout.vue
+++ src/vue/layouts/default/DefaultLayout.vue
@@ -1,12 +1,10 @@
 <template>
-  <WatchListProvider>
     <DefaultNavBar />
     <DefaultHeader />
     <DefaultPageContent>
       <slot></slot>
     </DefaultPageContent>
     <DefaultFooter />
-  </WatchListProvider>
 </template>
 
 <script lang="ts" setup>
@@ -14,5 +12,4 @@ import DefaultNavBar from "./components/DefaultNavBar.vue";
 import DefaultHeader from "./components/DefaultHeader.vue";
 import DefaultPageContent from "./components/DefaultPageContent.vue";
 import DefaultFooter from "./components/DefaultFooter.vue";
-import WatchListProvider from "../../components/WatchListProvider.vue";
 </script>
